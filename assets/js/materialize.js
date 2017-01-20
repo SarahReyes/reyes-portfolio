2a82bbb4e7d7cb41e443219700740e0a4796945c
SarahReyes <siwreyes@gmail.com>
2017-01-20 09:23:30 -0600

    refectoring, adding a row for work and contact

---
+++ assets/js/materialize.js
@@ -0,0 +1,13 @@
+
+
+// materialize features
+
+$(document).ready(function() {
+    // side bar nav
+	$('.button-collapse').sideNav({
+    menuWidth: 200, // Default is 240
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true // Closes side-nav on <a> clicks
+  }
+);
+});
