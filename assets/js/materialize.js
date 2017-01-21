3ef75ef461b08b7781a4e0fbe7118aed514a3893
SarahReyes <siwreyes@gmail.com>
2017-01-21 12:12:15 -0600

    adding portfolio row

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -5,9 +5,11 @@
 $(document).ready(function() {
     // side bar nav
 	$('.button-collapse').sideNav({
-    menuWidth: 200, // Default is 240
-    edge: 'left', // Choose the horizontal origin
-    closeOnClick: true // Closes side-nav on <a> clicks
-  }
-);
+    	menuWidth: 200, // Default is 240
+    	edge: 'left', // Choose the horizontal origin
+    	closeOnClick: true // Closes side-nav on <a> clicks
+  		}
+	);
+	// collapsible menus
+	 $('.collapsible').collapsible();
 });
